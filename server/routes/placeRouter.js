6af84057483654f724926961bbc74e499217a029
MichaelKaffel <michaeldkaffel@gmail.com>
2026-02-23 14:31:21 -0800

    The /places/:placeId/notes route implemented

--- server/routes/placeRouter.js
+++ server/routes/placeRouter.js
@@ -1,57 +1,118 @@
 const express = require('express');
 const Place = require('../models/places');
 
+
 const placeRouter = express.Router();
 
 placeRouter.route('/')
-.get((req, res, next) => {
-    Place.find()
-    .then(places => {
-        res.status(200).json(places);
+    .get((req, res, next) => {
+        Place.find()
+            .then(places => {
+                res.status(200).json(places);
+            })
+            .catch(err => next(err));
     })
-    .catch(err => next(err));
-})
-.post((req, res, next) => {
-    Place.create(req.body)
-    .then(place => {
-        console.log(`${req.body.kindOfPlace} created`, place);
-        res.status(200).json(place);
+    .post((req, res, next) => {
+        Place.create(req.body)
+            .then(place => {
+                console.log(`${req.body.kindOfPlace} created`, place);
+                res.status(200).json(place);
+            })
+            .catch(err => next(err));
     })
-    .catch(err => next(err));
-})
-.put((req, res) => {
-    res.status(403).end('PUT operation not supported on /places');
-})
-.delete((req, res) => {
-    res.status(403).end('DELETE operation not supported');
-})
+    .put((req, res) => {
+        res.status(403).end('PUT operation not supported on /places');
+    })
+    .delete((req, res) => {
+        res.status(403).end('DELETE operation not supported');
+    })
 
 placeRouter.route('/:placeId')
-.get((req, res, next) => {
-    Place.findById(req.params.placeId)
-    .then(place => {
-        res.status(200).json(place)
+    .get((req, res, next) => {
+        Place.findById(req.params.placeId)
+            .then(place => {
+                res.status(200).json(place)
+            })
+            .catch(err => next(err));
     })
-    .catch(err => next(err));
-})
-.post((req, res) => {
-    res.status(403).end(`POST operation not supported on /places/${req.params.placeId}`)
-})
-.put((req, res, next) => {
-    Place.findByIdAndUpdate(req.params.placeId, {
-        $set: req.body
-    }, { new: true })
-    .then(place => {
-        res.status(200).json(place)
+    .post((req, res) => {
+        res.status(403).end(`POST operation not supported on /places/${req.params.placeId}`)
     })
-    .catch(err => next(err));
-})
-.delete((req, res, next) => {
-    Place.findByIdAndDelete(req.params.placeId)
-    .then(response => {
-        res.status(200).json(response);
+    .put((req, res, next) => {
+        Place.findByIdAndUpdate(req.params.placeId, {
+            $set: req.body
+        }, { new: true })
+            .then(place => {
+                res.status(200).json(place)
+            })
+            .catch(err => next(err));
     })
-    .catch(err => next(err));
-});
+    .delete((req, res, next) => {
+        Place.findByIdAndDelete(req.params.placeId)
+            .then(response => {
+                res.status(200).json(response);
+            })
+            .catch(err => next(err));
+    });
+
+placeRouter.route('/:placeId/notes')
+    .get((req, res, next) => {
+        Place.findById(req.params.placeId)
+            .then(place => {
+                if (place) {
+                    res.status(200).json(place.notes)
+                } else {
+                    const err = new Error(`Place: ${req.params.placeId} not fount`);
+                    err.status = 404;
+                    return next(err);
+                }
+            })
+            .catch(err => next(err));
+    })
+    .post((req, res, next) => {
+        Place.findById(req.params.placeId)
+            .then(place => {
+                if (place) {
+                    place.notes.push(req.body);
+                    place.save()
+                        .then(place => {
+                            res.status(200).json(place);
+                        })
+                        .catch(err => next(err));
+                } else {
+                    const err = new Error(`Place: ${req.params.placeId} not found`);
+                    err.status = 404;
+                    return next(err);
+                }
+            })
+            .catch(err => next(err));
+    })
+    .put((req, res) => {
+        res.status(403).end(`PUT operation not supported on /places/${req.params.placeId}`)
+    })
+    .patch((req, res) => {
+        res.status(403).end(`PATCH operation not supported on /places/${req.params.placeId}`)
+    })
+    .delete((req, res, next) => {
+        Place.findById(req.params.placeId)
+            .then(place => {
+                if (place) {
+                    for (let i = (place.notes.length -1 ); i >= 0; i--) {
+                        place.notes.id(place.notes[i]._id).deleteOne();
+                    }
+                    place.save()
+                        .then(place => {
+                            res.status(200).json(place);
+                        })
+                        .catch(err => next(err))
+                } else {
+                    const err = new Error(`Place: ${req.params.placeId} not found`);
+                    err.status = 404;
+                    return next(err);
+                }
+            }) 
+            .catch(err => next(err));
+    });
+
 
 module.exports = placeRouter;
