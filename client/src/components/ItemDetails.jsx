3e90cdd15f8dead9fd813cc538a4834a61c01c00
MichaelKaffel <michaeldkaffel@gmail.com>
2026-03-11 14:41:08 -0700

    feat: Leaflet map display on ItemDetails with Open in Google Maps link

--- client/src/components/ItemDetails.jsx
+++ client/src/components/ItemDetails.jsx
@@ -1,13 +1,23 @@
-
 import Card from 'react-bootstrap/Card';
 import hikePlaceholderImg from '../app/images/hikesPlaceholder.png';
 import campsitePlaceholderImg from '../app/images/campsitesPlaceholder.png';
-import overlookPlaceholderImg from '../app/images/overlookPlaceholder.png'
+import overlookPlaceholderImg from '../app/images/overlookPlaceholder.png';
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import 'leaflet/dist/leaflet.css';
+import L from 'leaflet';
+
+delete L.Icon.Default.prototype._getIconUrl;
+L.Icon.Default.mergeOptions({
+    iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
+    iconUrl: require('leaflet/dist/images/marker-icon.png'),
+    shadowUrl: require('leaflet/dist/images/marker-shadow.png'),
+})
 
 const ItemDetails = ({ item }) => {
     const { imageUrl, description, location, title, kindOfPlace, dateVisited } = item;
 
     const formattedDate = new Date(dateVisited).toLocaleDateString();
+    const hasCoordinates = location?.lat && location?.lng
 
         let imageInsert;
 
@@ -34,6 +44,7 @@ const ItemDetails = ({ item }) => {
                 <Card.Text>
                     {description}
                 </Card.Text>
+                <hr/>
                 <div className='d-flex justify-content-between'>
                     <Card.Title className='text-start'>
                         {location?.name}
@@ -43,6 +54,36 @@ const ItemDetails = ({ item }) => {
                         <Card.Text className='fw-bold'>{formattedDate}</Card.Text>
                     </div>
                 </div>
+                {hasCoordinates && (
+                    <div className='mt-3'>
+                        
+                        <MapContainer
+                            center={[location.lat, location.lng]}
+                            zoom={12}
+                            style={{ height: '300px', width: '100%', borderRadius: '8px' }}
+                            scrollWheelZoom={false}
+                        >
+                            <TileLayer 
+                                url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
+                                attribution='© OpenStreetMap contributors'
+                            />
+                            <Marker position={[location.lat, location.lng]}>
+                                <Popup>{location.name || title}</Popup>
+                            </Marker>
+                        </MapContainer>
+
+                        
+                            
+                        <a
+                            href={`https://www.google.com/maps?q=${location.lat},${location.lng}`}
+                            target='_blank'
+                            rel='noopener noreferrer'
+                            className='btn btn-outline-secondary btn-sm mt-2'
+                        >
+                            Open in Google Maps
+                        </a>
+                    </div>
+                )}
             </Card.Body>
         </>
     )
